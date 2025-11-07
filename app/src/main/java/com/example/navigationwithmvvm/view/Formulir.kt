e7092f5834a616424f6b7756bc4e28aee211cae9
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 10:34:10 +0700

    buat nampilin tampilan utama form yang ada header di atasnya dan judulnya

--- app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
@@ -11,5 +11,15 @@ fun FormIsian(
     var nama by remember { mutableStateOf("") }
     var selectedJenis by remember { mutableStateOf(jenisK.first()) }
 
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Form Isian", color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
+    }
 }
