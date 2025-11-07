da726bd19cea3a5ab929650788f6551caa1242a8
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 13:29:10 +0700

    Menampilkan data

---
+++ app/src/main/java/com/example/navigationwithmvvm/view/TampilData.kt
@@ -0,0 +1,18 @@
+package com.example.navigationwithmvvm.view
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit
+) {
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+
+
+}
