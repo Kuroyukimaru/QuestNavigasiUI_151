69ba02193e7ee430423636b8bac08c69a07f1d67
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 10:28:54 +0700

    nyimpen isi yang diketik sama pilihan jenis kelamin

--- app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
@@ -1,11 +1,15 @@
 package com.example.navigationwithmvvm.view
 
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ) {
-    
+    var nama by remember { mutableStateOf("") }
+    var selectedJenis by remember { mutableStateOf(jenisK.first()) }
+
+
 }
