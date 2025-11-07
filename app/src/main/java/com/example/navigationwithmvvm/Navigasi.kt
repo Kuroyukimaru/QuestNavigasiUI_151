23004d31218583afd81e709651d2a8504f174f76
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 13:42:56 +0700

    buat untuk berpindah dari satu layar ke layar lain

--- app/src/main/java/com/example/navigationwithmvvm/Navigasi.kt
+++ app/src/main/java/com/example/navigationwithmvvm/Navigasi.kt
@@ -1,6 +1,14 @@
 package com.example.navigationwithmvvm
 
+
+
 enum class Navigasi {
     Formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
 }
