96841753e34d2aaa1ed65f85e182ef5a6ff93bf1
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-07 10:23:56 +0700

    Nambah FormIsian di file Formulir.kt

--- app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
+++ app/src/main/java/com/example/navigationwithmvvm/view/Formulir.kt
@@ -1,2 +1,11 @@
 package com.example.navigationwithmvvm.view
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    
+}
